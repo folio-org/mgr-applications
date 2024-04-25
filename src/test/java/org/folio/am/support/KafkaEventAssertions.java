1199c32b9b718e9bca9acb057a68b932e6bfccce
Pavel Filippov <pavel_filippov@epam.com>
2024-04-25 17:42:20 +0300

    MGRENTITLE-21 Fix kafka event assertions

--- src/test/java/org/folio/am/support/KafkaEventAssertions.java
+++ src/test/java/org/folio/am/support/KafkaEventAssertions.java
@@ -2,15 +2,14 @@ package org.folio.am.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.am.integration.kafka.DiscoveryPublisher.DISCOVERY_DESTINATION;
+import static org.folio.common.utils.CollectionUtils.mapItems;
 import static org.folio.integration.kafka.KafkaUtils.getEnvTopicName;
 import static org.folio.test.FakeKafkaConsumer.getEvents;
-import static org.hamcrest.Matchers.hasSize;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ConditionFactory;
@@ -26,12 +25,19 @@ public final class KafkaEventAssertions {
 
   public static void assertDiscoveryEvents(String... moduleIds) {
     var topic = getEnvTopicName(DISCOVERY_DESTINATION);
-    var kafkaEvents = await().until(() -> getEvents(topic, DiscoveryEvent.class), hasSize(moduleIds.length));
     var expectedEvents = Arrays.stream(moduleIds).map(DiscoveryEvent::new).collect(Collectors.toList());
-    assertThat(kafkaEvents).map(ConsumerRecord::value).containsExactlyInAnyOrderElementsOf(expectedEvents);
+    await().untilAsserted(() -> {
+      var consumerRecords = getEvents(topic, DiscoveryEvent.class);
+      var entitlementEvents = mapItems(consumerRecords, ConsumerRecord::value);
+      assertThat(entitlementEvents).containsSequence(expectedEvents);
+    });
   }
 
   public static void assertNoDiscoveryEvents() {
-    assertDiscoveryEvents(ArrayUtils.EMPTY_STRING_ARRAY);
+    var topic = getEnvTopicName(DISCOVERY_DESTINATION);
+    await().untilAsserted(() -> {
+      var consumerRecords = getEvents(topic, DiscoveryEvent.class);
+      assertThat(consumerRecords).isEmpty();
+    });
   }
 }
