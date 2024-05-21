71af7f55d3cead170c89b7b4ec414f07897e9bf3
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2024-05-21 19:02:31 +0300

    specify java security properties in dedicated file copy the file into docker image and use it during app start up

--- src/main/java/org/folio/am/ApplicationManagerApplication.java
+++ src/main/java/org/folio/am/ApplicationManagerApplication.java
@@ -1,5 +1,8 @@
 package org.folio.am;
 
+import java.security.Security;
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +15,8 @@ public class ApplicationManagerApplication {
    * @param args command line arguments.
    */
   public static void main(String[] args) {
+    Security.addProvider(new BouncyCastleFipsProvider());
+    Security.addProvider(new BouncyCastleJsseProvider("fips:BCFIPS"));
     SpringApplication.run(ApplicationManagerApplication.class, args);
   }
 }
