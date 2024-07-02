35da1acc1de9769df0941708e9b10c96f7f164a2
Yauhen Vavilkin <yauhen_vavilkin@epam.com>
2024-07-02 11:00:02 +0400

    fix typos

--- src/main/java/org/folio/am/service/ModuleDiscoveryService.java
+++ src/main/java/org/folio/am/service/ModuleDiscoveryService.java
@@ -11,7 +11,6 @@ import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ForkJoinPool;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections4.ListUtils;
