7ce97d0b045e209b3ba13206d337b2a84d26e9e8
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2024-11-04 12:54:25 +0200

    replace startsWith() with containsString()

--- src/test/java/org/folio/am/controller/ApiExceptionHandlerTest.java
+++ src/test/java/org/folio/am/controller/ApiExceptionHandlerTest.java
@@ -2,6 +2,7 @@ package org.folio.am.controller;
 
 import static org.folio.am.support.TestConstants.ROLE_ADMIN;
 import static org.folio.am.support.TestConstants.ROLE_USER;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.Mockito.when;
@@ -223,7 +224,7 @@ class ApiExceptionHandlerTest {
         .header(CACHE_CONTROL, "no-cache"))
       .andExpect(status().isBadRequest())
       .andExpect(jsonPath("$.total_records", is(1)))
-      .andExpect(jsonPath("$.errors[0].message", startsWith(errorMessage)))
+      .andExpect(jsonPath("$.errors[0].message", containsString(errorMessage)))
       .andExpect(jsonPath("$.errors[0].type", is("MethodArgumentTypeMismatchException")))
       .andExpect(jsonPath("$.errors[0].code", is("validation_error")));
   }
