7909ae5db79db5d4b40499e9892ebf8d958311e4
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-09-10 14:11:48 +0300

    feat(validation): add negative test cases for application descriptor validation

--- src/test/java/org/folio/am/it/ApplicationValidateInFarModeIT.java
+++ src/test/java/org/folio/am/it/ApplicationValidateInFarModeIT.java
@@ -1,7 +1,10 @@
 package org.folio.am.it;
 
+import static java.lang.String.format;
+import static org.folio.test.TestUtils.copy;
 import static org.folio.test.TestUtils.parse;
 import static org.folio.test.TestUtils.readString;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -11,6 +14,7 @@ import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.folio.am.domain.dto.ApplicationDescriptor;
 import org.folio.am.domain.dto.ApplicationDescriptorsValidation;
+import org.folio.am.exception.RequestValidationException;
 import org.folio.am.support.extensions.EnablePostgres;
 import org.folio.test.base.BaseBackendIntegrationTest;
 import org.folio.test.extensions.EnableKafka;
@@ -48,11 +52,9 @@ class ApplicationValidateInFarModeIT  extends BaseBackendIntegrationTest {
 
   @Test
   void validateDescriptors_positive_allInRequest() throws Exception {
-    var req = new ApplicationDescriptorsValidation(List.of(
-      APP_PLATFORM_MINIMAL, APP_PLATFORM_COMPLETE, APP_EHOLDINGS, APP_INN_REACH));
+    var req = validationReq(APP_PLATFORM_MINIMAL, APP_PLATFORM_COMPLETE, APP_EHOLDINGS, APP_INN_REACH);
 
     attemptPost("/applications/validate-descriptors", req)
-      .andDo(logResponseBody())
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.length()", is(4)))
       .andExpect(jsonPath("$.[0]", is(APP_EHOLDINGS.getId())))
@@ -65,11 +67,9 @@ class ApplicationValidateInFarModeIT  extends BaseBackendIntegrationTest {
   void validateDescriptors_positive_oneStoredAnotherInRequest() throws Exception {
     doPost("/applications", APP_PLATFORM_MINIMAL);
 
-    var req = new ApplicationDescriptorsValidation(List.of(
-      APP_PLATFORM_COMPLETE, APP_EHOLDINGS, APP_INN_REACH));
+    var req = validationReq(APP_PLATFORM_COMPLETE, APP_EHOLDINGS, APP_INN_REACH);
 
     attemptPost("/applications/validate-descriptors", req)
-      .andDo(logResponseBody())
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.length()", is(4)))
       .andExpect(jsonPath("$.[0]", is(APP_EHOLDINGS.getId())))
@@ -83,15 +83,73 @@ class ApplicationValidateInFarModeIT  extends BaseBackendIntegrationTest {
     doPost("/applications", APP_PLATFORM_MINIMAL);
     doPost("/applications", APP_PLATFORM_COMPLETE);
 
-    var req = new ApplicationDescriptorsValidation(List.of(APP_EHOLDINGS, APP_INN_REACH));
+    var req = validationReq(APP_EHOLDINGS, APP_INN_REACH);
 
     attemptPost("/applications/validate-descriptors", req)
-      .andDo(logResponseBody())
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.length()", is(4)))
       .andExpect(jsonPath("$.[0]", is(APP_EHOLDINGS.getId())))
       .andExpect(jsonPath("$.[1]", is(APP_INN_REACH.getId())))
       .andExpect(jsonPath("$.[2]", is(APP_PLATFORM_COMPLETE.getId())))
       .andExpect(jsonPath("$.[3]", is(APP_PLATFORM_MINIMAL.getId())));
+  }
+
+  @Test
+  void validateDescriptors_negative_duplicatedApplication() throws Exception {
+    var anotherAppPlatformMinimal = copy(APP_PLATFORM_MINIMAL)
+      .id(APP_PLATFORM_MINIMAL.getName() + "-0.0.1-test")
+      .version("0.0.1-test");
+
+    var req = validationReq(APP_PLATFORM_MINIMAL, anotherAppPlatformMinimal, APP_PLATFORM_COMPLETE);
+
+    attemptPost("/applications/validate-descriptors", req)
+      .andExpectAll(validationErr(
+        RequestValidationException.class.getSimpleName(),
+        "Duplicate application descriptor with the same name in the request",
+        "name", APP_PLATFORM_MINIMAL.getName()));
+  }
+
+  @Test
+  void validateDescriptors_negative_unsatisfiedDependency() throws Exception {
+    var req = validationReq(APP_PLATFORM_MINIMAL, APP_EHOLDINGS, APP_INN_REACH);
+
+    attemptPost("/applications/validate-descriptors", req)
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.errors[0].message",
+        containsString("Cannot find application which satisfies the dependency")))
+      .andExpect(jsonPath("$.errors[0].code", is("validation_error")))
+      .andExpect(jsonPath("$.errors[0].type", is(RequestValidationException.class.getSimpleName())))
+      .andExpect(jsonPath("$.errors[0].parameters[0].key", is("dependencyName")))
+      .andExpect(jsonPath("$.errors[0].parameters[0].value", is(APP_PLATFORM_COMPLETE.getName())))
+      .andExpect(jsonPath("$.errors[0].parameters[1].key", is("dependencyVersion")))
+      .andExpect(jsonPath("$.errors[0].parameters[1].value", is("^3.0.0-SNAPSHOT")))
+      .andExpect(jsonPath("$.total_records", is(1)));
+  }
+
+  @Test
+  void validateDescriptors_negative_versionMismatch() throws Exception {
+    var appEholdings = copy(APP_EHOLDINGS);
+    var dependencyVersion = "0.0.1-test";
+    appEholdings.getDependencies().getFirst().setVersion(dependencyVersion);
+
+    var req = validationReq(APP_PLATFORM_MINIMAL, APP_PLATFORM_COMPLETE, appEholdings, APP_INN_REACH);
+
+    attemptPost("/applications/validate-descriptors", req)
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.errors[0].message",
+        containsString(format("Dependency version range '%s' " +
+          "is not satisfied by already resolved application '%s' with version '%s'",
+          dependencyVersion, APP_PLATFORM_MINIMAL.getName(), APP_PLATFORM_MINIMAL.getVersion()))))
+      .andExpect(jsonPath("$.errors[0].code", is("validation_error")))
+      .andExpect(jsonPath("$.errors[0].type", is(RequestValidationException.class.getSimpleName())))
+      .andExpect(jsonPath("$.errors[0].parameters[0].key", is("dependencyName")))
+      .andExpect(jsonPath("$.errors[0].parameters[0].value", is(APP_PLATFORM_MINIMAL.getName())))
+      .andExpect(jsonPath("$.errors[0].parameters[1].key", is("dependencyVersion")))
+      .andExpect(jsonPath("$.errors[0].parameters[1].value", is(dependencyVersion)))
+      .andExpect(jsonPath("$.total_records", is(1)));
+  }
+
+  private static ApplicationDescriptorsValidation validationReq(ApplicationDescriptor... descriptors) {
+    return new ApplicationDescriptorsValidation(List.of(descriptors));
   }
 }
