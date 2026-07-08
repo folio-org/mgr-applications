08e4c1b7119e613060ccb28843aa7e65b401dd64
Oleksii Kuzminov <40833111+OleksiiKuzminov@users.noreply.github.com>
2026-07-08 13:23:50 +0200

    EUREKA-899: POC - Application-scoped sidecars bootstrap (#267)
    (cherry picked from commit 3c7b4f3c7c3acca83a23436de75e1ce90b3cbf33)

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+## Version `v4.0.2` (IN PROGRESS)
+* Application-scoped sidecar bootstrap: ingress and egress module-bootstrap endpoints (EUREKA-899)
+
+---
+
 ## Version `v4.0.1` (27.04.2026)
 * Allow interface to be both provided and require / optional in the same module descriptor (MGRAPPS-99)
 
--- src/main/java/org/folio/am/controller/ModuleBootstrapController.java
+++ src/main/java/org/folio/am/controller/ModuleBootstrapController.java
@@ -1,6 +1,8 @@
 package org.folio.am.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.folio.am.domain.dto.EgressBootstrap;
+import org.folio.am.domain.dto.EgressBootstrapRequest;
 import org.folio.am.domain.dto.ModuleBootstrap;
 import org.folio.am.rest.resource.ModuleBootstrapApi;
 import org.folio.am.service.ModuleBootstrapService;
@@ -18,5 +20,15 @@ public class ModuleBootstrapController extends BaseController implements ModuleB
     var moduleBootstrap = service.getById(id);
 
     return ResponseEntity.ok(moduleBootstrap);
+  }
+
+  @Override
+  public ResponseEntity<ModuleBootstrap> getModuleIngressBootstrap(String id) {
+    return ResponseEntity.ok(service.getIngressBootstrap(id));
+  }
+
+  @Override
+  public ResponseEntity<EgressBootstrap> getModuleEgressBootstrap(String id, EgressBootstrapRequest request) {
+    return ResponseEntity.ok(service.getEgressBootstrap(id, request.getApplicationIds()));
   }
 }
